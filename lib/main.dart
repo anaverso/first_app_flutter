a55dad44100d02288254fae832e255c9fbcc9f5f
Ana Kiara <kiara.medeiros@itriad.org.br>
2025-04-22 19:29:47 -0400

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 42, 8, 101),
-                Color.fromARGB(255, 110, 93, 139),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white, 
-                fontSize: 28
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(colors: [Colors.deepPurple, Colors.lightBlue]),
       ),
     ),
   );
